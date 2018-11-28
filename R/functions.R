e53ea85a1204d50cff79a185f9e23cdc9b13a062
Susanna Marquez <susanna.marquez@yale.edu>
2018-11-28 13:55:54 -0500

    Ignore . when counting mutated positions

--- R/functions.R
+++ R/functions.R
@@ -1821,7 +1821,7 @@ positionMutations <- function(data, germline, pos_range){
     pos_db = pos_db %>%
         mutate_(NT = ~substring(SEQUENCE_IMGT, POSITION, POSITION)) %>%
         mutate_(GERM_NT = ~substring(germline, POSITION, POSITION)) %>%
-        mutate_(MUTATED = ~(NT != GERM_NT & NT != "N" & NT != "-" & NT != "")) %>%
+        mutate_(MUTATED = ~(NT != GERM_NT & NT != "N" & NT != "-" & NT != "." & NT != "")) %>%
         mutate_(OBSERVED = ~(NT != "-" & NT != ""))
     return(pos_db)
 }
